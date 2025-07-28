46572858190d92a0e269053a2b8469f63abc9cee
techzalak <zalakgandhi20@gmail.com>
2025-07-28 23:22:37 +0530

    ProductListViewModel

--- ViewModels/ProductListViewModel.cs
+++ ViewModels/ProductListViewModel.cs
@@ -1,18 +1,29 @@
-﻿using ZalakProject.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ZalakProject.Models;
 
 namespace ZalakProject.ViewModels
 {
     public class ProductListViewModel
     {
-        public List<Product> Products { get; set; } = new List<Product>();
-        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
-        public string? SearchTerm { get; set; }
-        public int? CategoryId { get; set; }
-        public double? Price { get; set; }
-        public int? MinRating { get; set; }
-        public int CurrentPage { get; set; } = 1;
-        public int TotalPages { get; set; }
-        public int PageSize { get; set; } = 12;
-        public int TotalProducts { get; set; }
-    }
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Required]
+        public int StockQuantity{get; set; }
+        public int CategoryId { get; set; }
+        [Required]
+        public string ProductImage {  get; set; }
+        public string SellerId { get; set; }
 }
