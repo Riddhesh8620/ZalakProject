8b3919a7e61e612fb1ff8f73ffef7a18b3c42345
RIDDHESH PINGLE <riddhesh.pingle2000@gmail.com>
2025-07-28 21:27:28 +0530

    Commit  for Routing to dahsboard based on Roles

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -113,6 +113,19 @@ namespace ZalakProject.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Contains("Admin"))
+                    return LocalRedirect("/Admin/Dashboard");
+                else if (roles.Contains("Seller"))
+                    return LocalRedirect("/Seller/Dashboard");
+                else if (roles.Contains("Buyer"))
+                    return LocalRedirect("/Buyer/Dashboard");
+                else
+                    return LocalRedirect(returnUrl ?? "/");
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
