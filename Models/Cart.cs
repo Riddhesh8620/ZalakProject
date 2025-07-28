4b013f556d7b1e6d281c6314593f0a8f6f076cc7
techzalak <zalakgandhi20@gmail.com>
2025-07-29 04:01:18 +0530

    Cart Model

---
+++ Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZalakProject.Models
+{
+    public class Cart
+    {
+        [Required]
+        public string ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+        [Required]
+        public string SellerId  { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+    }
+}
